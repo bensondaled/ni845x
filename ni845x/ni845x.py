0bde4ff68e57aeb8d8bd2a33bcde83ece054f6c6
bensondaled <bendeverett@gmail.com>
2016-04-04 14:45:08 -0400

    working well

--- ni845x/ni845x.py
+++ ni845x/ni845x.py
@@ -3,26 +3,24 @@ import numpy as np
 
 MAX_SIZE = 1024
 DEV_SIZE = 256
+dll = ctypes.windll.LoadLibrary('Ni845x.dll')
 
 class Ni845xError(Exception):
     def __init__(self, status_code):
 
-        cdef bytes message
-        cdef char status_string[MAX_SIZE]
+        message = ctypes.create_string_buffer(MAX_SIZE)
         
-        ni845xStatusToString(status_code, MAX_SIZE, status_string)
-        message = status_string
+        dll.ni845xStatusToString(status_code, MAX_SIZE, message)
         
-        Exception.__init__(self, message)
+        Exception.__init__(self, message.value)
         
 def errChk(err):
     if err:
         raise Ni845xError(err)
 
+
 class NI845x():
     def __init__(self):
-
-        self.dll = ctypes.windll.LoadLibrary('Ni845x.dll')
        
         # Determine available devices
         NextDevice = ctypes.create_string_buffer(DEV_SIZE)
@@ -30,15 +28,18 @@ class NI845x():
         NumberFound = ctypes.c_int32()
         
         self._DeviceHandle = 0
-        self._I2CHandle = 0
         
-        errChk(self.dll.ni845xFindDevice(NextDevice, &FindDeviceHandle, &NumberFound))
-            
-        self.devices_list = [NextDevice]
-        
-        for i in range(NumberFound-1):
-            errChk(ni845xFindDeviceNext(FindDeviceHandle, NextDevice))
-            self.devices_list.append(NextDevice)
-        
-        self._name = self.devices_list[0]
+        errChk(dll.ni845xFindDevice(ctypes.byref(NextDevice), ctypes.byref(FindDeviceHandle), ctypes.byref(NumberFound)))
 
+        if NumberFound.value != 1:
+            raise Exception('Only implemented support for exactly 1 USB card. {} found.'.format(NumberFound.value))
+        self._name = NextDevice
+        
+        self._open()
+
+    def _open(self):
+        self.dev_handle = ctypes.c_int32()
+        errChk(dll.ni845xOpen(ctypes.byref(self._name), ctypes.byref(self.dev_handle)))
+        
+    def end(self):
+        errChk(dll.ni845xClose(self.dev_handle))
