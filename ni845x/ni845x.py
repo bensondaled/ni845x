bfe14c65b6e1f2eb21a4620785ac4a73a9be4c8d
bensondaled <bendeverett@gmail.com>
2016-04-05 17:56:01 -0400

    working for dio

--- ni845x/ni845x.py
+++ ni845x/ni845x.py
@@ -3,6 +3,7 @@ import numpy as np
 
 MAX_SIZE = 1024
 DEV_SIZE = 256
+
 dll = ctypes.windll.LoadLibrary('Ni845x.dll')
 
 class Ni845xError(Exception):
@@ -20,15 +21,19 @@ def errChk(err):
 
 
 class NI845x():
+    VOLTS33 = 33
+    VOLTS25 = 25
+    VOLTS18 = 18
+    VOLTS15 = 15
+    VOLTS12 = 12
+    INPUT,OUTPUT = 0,1
     def __init__(self):
        
         # Determine available devices
         NextDevice = ctypes.create_string_buffer(DEV_SIZE)
         FindDeviceHandle = ctypes.c_int32()
         NumberFound = ctypes.c_int32()
-        
-        self._DeviceHandle = 0
-        
+                
         errChk(dll.ni845xFindDevice(ctypes.byref(NextDevice), ctypes.byref(FindDeviceHandle), ctypes.byref(NumberFound)))
 
         if NumberFound.value != 1:
@@ -36,56 +41,34 @@ class NI845x():
         self._name = NextDevice
         
         self._open()
+        self.set_io_voltage_level(self.VOLTS33)
+        self.set_port_line_direction_map(self.OUTPUT*np.ones(8))
 
     def _open(self):
         self.dev_handle = ctypes.c_int32()
         errChk(dll.ni845xOpen(ctypes.byref(self._name), ctypes.byref(self.dev_handle)))
+    
+    def set_port_line_direction_map(self, mapp, port=0):
+        # mapp: np array or list with 8 0's or 1's
+        # 0 = input, 1 = output
+        port = ctypes.c_uint8(port)
+        mapp = np.asarray(mapp)
+        assert len(mapp)==8
+        r = np.arange(7,-1,-1)
+        _map = np.sum(2**r * mapp).astype(int)
+        bitmap = ctypes.c_uint8(_map)
+        errChk(dll.ni845xDioSetPortLineDirectionMap(self.dev_handle, port, bitmap))
         
+    def set_io_voltage_level(self, lev):
+        lev = ctypes.c_uint8(lev)
+        errChk(dll.ni845xSetIoVoltageLevel(self.dev_handle, lev))
+    
     def end(self):
         errChk(dll.ni845xClose(self.dev_handle))
 
-    def configure_i2c(self, size=dll.kNi845xI2cAddress7Bit, address=0, clock_rate=100):
-        """
-        Set the ni845x I2C configuration.
+    def write_dio(self, line, val, port=0):
+        line = ctypes.c_uint8(line)
+        port = ctypes.c_uint8(port)
+        val = ctypes.c_int32(val)
+        errChk(dll.ni845xDioWriteLine( self.dev_handle, port, line, val ))
         
-        Parameters
-        ----------
-            size : Configuration address size (default 7Bit).
-            address : Configuration address (default 0).
-            clock_rate : Configuration clock rate in kilohertz (default 100).
-
-        Returns
-        -------
-            None
-        """
-        
-        #
-        # create configuration reference
-        #
-        errChk(ni845xI2cConfigurationOpen(&self._I2CHandle))
-        
-        #
-        # configure configuration properties
-        #
-        errChk(ni845xI2cConfigurationSetAddressSize(self._I2CHandle, size))
-        errChk(ni845xI2cConfigurationSetAddress(self._I2CHandle, address))
-        errChk(ni845xI2cConfigurationSetClockRate(self._I2CHandle, clock_rate))
-        
-    def I2cClose(self):
-        """
-        Release the handles to the ni845x I2C interface.
-
-        Parameters
-        ----------
-            None
-
-        Returns
-        -------
-            None
-        """
-
-        if self._I2CHandle:
-            ni845xI2cConfigurationClose(self._I2CHandle)
-            
-        if self._DeviceHandle:
-            ni845xClose(self._DeviceHandle)
